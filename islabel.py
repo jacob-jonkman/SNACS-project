0d6cd0a82e619f73ad1fa5c19102fa2abef5ad9d
J.J. Koschny <s1417452@leidenuniv.nl>
2017-11-21 15:07:00 +0100

    Updated

--- islabel.py
+++ islabel.py
@@ -1,10 +1,156 @@
+"""
+Implementation of the IS-Label algorithm
 
+Assumptions:
+- Independent sets are chosen randomly with a given maximum set size.
+  Not necessarily the best method. Should be investigated.
+
+Todo:
+- Investigate optimal IS generation
+- gen_label
+- Change lists to np arrays (would require calculating upper bounds and
+  dealing with unset values, but perhaps worth it)
+
+"""
+import numpy as np
 import networkx as nx
+import argparse as ap
+
+
+'''
+Returns the weight of the edge (n, m) in the highest level less than
+the parameter level. Returns None if the edge does not exist.
+'''
+def get_weight(network, n, m, level):
+	for i in np.arange(level)[::-1]:
+		if i in network[n][m]:
+			return network[n][m][i]
+	return None
+
+'''
+Adds an augmenting edge (n, m) if necessary.
+'''
+def augment(network, level, n, m, o):
+	new_dist = get_weight(network, n, m, level)
+	new_dist += get_weight(network, m, o, level)
+	
+	if get_weight(network, n, o, level) > new_dist:
+		network[n][o][level] = new_dist
+	else:
+		network.add_edge(n, o, attr_dict={level : new_dist})
+
+'''
+Adds all necessary aumenting edges to the graph after removing the
+given level.
+'''
+def add_augmenting_edges(network, level):
+	for node in level:
+		for n, m in network[node], network[node]:
+			if n < m:
+				augment(network, level, n, node, m)
+
+'''
+Generates a list of adjacent nodes for each node in the network.
+Then it generates a list of these lists, which is sorted by degree
+of the nodes (or: the number of adjacent nodes in each list).
+'''
+def sorted_adjs(network):
+	adjacents = list()
+	for i in np.arange(network.number_of_nodes()):
+		for j in np.arange(len(adjacents) + 1):
+			if j > len(adjacents) or len(network[i]) < len(adjacents[j][1]):
+				adjacents.insert(j, (i, network[i]))
+	
+	return adjacents
+
+'''
+Generates one level in the hierarchy required to calculate the labels.
+Such a level corresponds to an L_i in the paper.
+'''
+def gen_level(network):
+	new_level = list()
+	gprime = sorted_adjs(network)
+	lprime = list()
+	adjacents = list()
+	
+	for node, adj in gprime:
+		if not node in lprime:
+			new_level.append(node)
+			adjacents.append(network[node])
+			for m in network[node]:
+				if not m in lprime:
+					lprime.append(m)
+		
+	return [new_level, adjacents]
+	
+def gen_subnet(network, level, adjacents):
+	subnet = network.copy()
+	
+	return subnet
+
+'''
+Generates the hierarchy required to calculate the labels for each node.
+'''
+def gen_hierarchy(network):
+	adj = [network[0]]
+	levels = list()
+	subnets = [network]
+	while len(adj) > 0:
+		[level, adj] = gen_level(subnets[-1])
+		levels.append(level)
+		neti = gen_subnet(subnets[-1], level, adj)
+		subnets.append(neti)
+		
+	return levels, subnets
+	
+'''
+Generates the label of the given node using the given hierarchy
+'''
+def gen_label(network, hierarchy, node):
+	return
+	
+	
+def init_rand(seed):
+	np.random.seed(seed)
+
+
+def parse_args():
+	parser = ap.ArgumentParser(description='Run islabel preprocessing.')
+	parser.add_argument('fin',
+											help='path to the file containing the network')
+	parser.add_argument('--directed',
+	                    help=('provide this flag to interpret the ' +
+	                    'edgelist as a directed network'),
+	                    action='store_true')
+	parser.add_argument('--weight',
+	                    help=('provide this flag to interpret the ' +
+	                    'edgelist as a weighted network'),
+	                    action='store_true')
+	parser.add_argument('--seed',
+											help='Seed for the random number generator')
+	return parser.parse_args()
 
 
 def main():
-	
-	return
+	args = parse_args()
+	if args.weight:
+		data = ((0, float),)
+	else:
+		data = None
+		
+	if args.seed:
+		init_rand(seed)
+	else:
+		init_rand(0)
+		
+	if(args.directed):
+		network = nx.read_edgelist(args.fin,
+															 create_using=nx.DiGraph(), data=data)
+		print("Successfully loaded directed network.")
+	else:
+		network = nx.read_edgelist(args.fin, data=data)
+		print("Successfully loaded undirected network.")
+
 
 if __name__ == "__main__":
 	main()
