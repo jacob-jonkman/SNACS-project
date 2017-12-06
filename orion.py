0a69cdfa360a4543fbf02f106557bdd3d4f35c20
Jacob <jacob.jonkman@gmail.com>
2017-12-06 23:12:13 +0100

    kleine slordigheden weggepoetst

--- orion.py
+++ orion.py
@@ -2,7 +2,6 @@ import networkx as nx
 import numpy as np
 import math
 from scipy.optimize import fmin
-from scipy.spatial import distance
 
 num_landmarks = 100
 initial = 16
@@ -16,7 +15,8 @@ def choose_initial_landmarks(G):
 	sortedlist = sorted(degreelist, key=lambda tup:int(tup[1]), reverse=True)
 	return sortedlist[:num_landmarks], sortedlist[num_landmarks:]
 
-# Compute actual distance between all landmark nodes
+
+# Compute actual distance between all landmark nodes using BFS
 def compute_landmark_distances(landmarks, num_nodes, G):
 	distances = np.zeros((num_landmarks, num_nodes))
 	i=0
@@ -26,13 +26,17 @@ def compute_landmark_distances(landmarks, num_nodes, G):
 		sorted_dists = np.array(sorted(dists.items(), key=lambda x:int(x[0])))
 		distances[i,:] = sorted_dists[:,1]
 		i+=1
-		
 	return distances
+
 	
+# Splits the distance matrix into two matrices: 
+# -landmarkcols: distance from landmarks to other landmarks
+# -regularcols: distance from landmarks to regular nodes
 def split_distances(all_distances, landmark_indices, regular_indices):
 	landmarkcols = all_distances[:,landmark_indices]
 	regularcols = all_distances[:,regular_indices]	
 	return landmarkcols, regularcols
+
 	
 # Objective function to be minimized by fmin used in initial_coordinates()
 def diff_initial(coordinates, distances):
@@ -43,49 +47,50 @@ def diff_initial(coordinates, distances):
 			summed = (coordinates[i,:] - coordinates[j,:])**2
 			euclidean = math.sqrt(sum(summed))
 			difference += np.abs(distances[i,j]-euclidean)
-	
-	#print(difference)
 	return difference
+
 
 # Apply simplex downhill algorithm to optimize coordinates of first 16 landmarks
 def initial_coordinates(landmarks, landmark_distances):
 	coordinates = np.random.rand(initial, D)*2
 	return fmin(func=diff_initial, x0=coordinates, args=(landmark_distances,), xtol=xtol, ftol=ftol, maxfun=1000000)
 
+
 # Objective function to be minimized by fmin used in second_coordinates()
 def diff_second(second_coordinates, first_coordinates, distances):
 	second_coordinates = second_coordinates.reshape(num_landmarks-initial, D)
-	
 	difference = 0
 	for i in np.arange(len(second_coordinates)):
 		for j in np.arange(len(first_coordinates)):
 			sums = (second_coordinates[i,:]-first_coordinates[j,:])**2
 			euclidean = math.sqrt(sum(sums))
 			difference += np.abs(distances[i,j]-euclidean)
-	#print(difference)	
 	return difference
+
 
 # Find coordinates of the other landmarks by using coordinates of first landmark nodes
 def second_coordinates(first_coordinates, distances):
 	coordinates = np.random.rand(num_landmarks-initial, D)*2
 	return fmin(func=diff_second, x0=coordinates, args=(first_coordinates,distances,), xtol=xtol, ftol=ftol, maxfun=1000000)
 
+
+# Objective function to be minimized by fmin used in regular_coordinates()
 def diff_regular(coordinates, landmark_coordinates, distances, num_nodes):
 	difference = 0
-	coordinates = coordinates.reshape(num_nodes-num_landmarks, D)
-	
+	coordinates = coordinates.reshape(num_nodes-num_landmarks, D)	
 	for i in np.arange(num_nodes-num_landmarks):
 		for j in np.arange(num_landmarks):
 			sums = (coordinates[i,:] - landmark_coordinates[j,:])**2
 			euclidean = math.sqrt(sum(sums))
 			difference += np.abs(distances[j,i] - euclidean)
-	print(difference)
 	return difference
+
 
 # Find coordinates of the regular ndoes by using coordinates of all landmark nodes
 def regular_coordinates(landmark_coordinates, distances, num_nodes):
 	coordinates = np.random.rand(num_nodes - num_landmarks, D)*4
 	return fmin(func=diff_regular, x0=coordinates, args=(landmark_coordinates,distances,num_nodes,), xtol=xtol, ftol=ftol, maxfun=100000)
+
 
 def main():
 	np.random.seed(42)
@@ -93,32 +98,39 @@ def main():
 	#G = nx.gnm_random_graph(200, 1000, seed=42, directed=False)
 	G = nx.read_edgelist("facebook_combined.txt")
 	num_nodes = G.number_of_nodes()
-	print(num_nodes)
+	num_edges = G.number_of_edges()
+	print("Succesfully loaded network with", num_nodes, "nodes and", num_edges, "edges.")
 	
 	# Split the nodes in landmark nodes and regular nodes, and find the corresponding indices
 	landmarks, regular_nodes = choose_initial_landmarks(G)
-	landmark_indices = np.array([int(landmark[0]) for landmark in landmarks])
-	regular_indices = np.array([int(node[0]) for node in regular_nodes])
+	landmark_indices = [int(landmark[0]) for landmark in landmarks]
+	regular_indices = [int(node[0]) for node in regular_nodes]
 	
 	# Find the distance from each landmark nodes to all other nodes
 	all_distances = compute_landmark_distances(landmarks, num_nodes, G)
 	landmark_distances, regular_distances = split_distances(all_distances, landmark_indices, regular_indices)
 	
+	# Start adding the nodes in three steps:
+	# 1: Add the initial landmark nodes
+	# 2: Add the remaining landmark nodes
+	# 3: Add all the regular nodes
 	print("Start finding coordinates for initial", initial, "landmark nodes")
 	first_coords = initial_coordinates(landmarks[:initial], landmark_distances[:initial]).reshape(initial,  D)
 	
 	print("Start finding coordinates for the other", num_landmarks-initial, "landmark nodes")
 	second_coords = second_coordinates(first_coords, landmark_distances[initial:num_landmarks]).reshape(num_landmarks-initial, D)
 	
-	landmark_coords = np.append(first_coords, second_coords).reshape(num_landmarks, D) # All landmark coordinates
+	# Combine landmark coordinates in single array
+	landmark_coords = np.append(first_coords, second_coords).reshape(num_landmarks, D)
 	
-	# !!! Mogelijk moet regular distances nog getransposet worden (was dists_to_landmarks) !!! #
 	print("Start finding coordinates for the", num_nodes-num_landmarks, "regular nodes")
-	regular_coords = regular_coordinates(landmark_coords, regular_distances, num_nodes).reshape(num_nodes-num_landmarks, D)
+	regular_coords = regular_coordinates(landmark_coords, regular_distances, num_nodes).reshape(num_nodes-num_landmarks, D)	
 	
+	# Combine all coordinates in single array
 	all_coordinates = np.append(landmark_coords, regular_coords).reshape(num_nodes, D)
 	
 	np.save("coordinates_facebook_small.npy", all_coordinates)
+
 
 if __name__ == "__main__":
 	main()
